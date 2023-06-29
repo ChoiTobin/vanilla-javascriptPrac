fc7e135d447219c972388b5f564079183e14ecc3
rorobin <rovello2@gmail.com>
2023-06-29 19:33:44 +0900

    키보드업데이트

--- file/src/pages/Home.jsx
+++ file/src/pages/Home.jsx
@@ -4,78 +4,81 @@ import {useState} from 'react';
 import { useEffect } from 'react';
 
 export const Home = () => {
-    const [sum,setSum] = useState('')
-    const [result,setResult] = useState('')
-    
-    useEffect(()=>{
-        window.addEventListener("keydown",handleKeyDown)
-    })
-
-
-    const handleNumClick = (event) => {
-        console.log(event)
-        const buttonValue = event.target.innerText; 
-        // span은 event.target.value X 
+  const [sum, setSum] = useState('');
+  const [result, setResult] = useState('');
+  
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [result]);
  
-        setSum(sum + buttonValue) 
-            console.log(sum)
-        };
+  const handleNumClick = (event) => {
+      const buttonValue = event.target.innerText;
+      setSum(prevSum => prevSum + buttonValue);
+  };
+  
+  const handleCalClick = () => {
+      try {
+          let calculationResult = eval(sum);
+          setResult(calculationResult);
+      } catch (error) {
+          console.log('잘못된 수식입니다.');
+      }
+  };
+  
+  const handleKeyDown = (e) => {
 
-        const handleCalClick = () => {
-            try {
-
-               let remove = eval(sum);
-                setResult(remove) 
-            } catch (error) {
-              console.log('잘못된 수식입니다.');
-            }
-          };
+      if (e.key === "Enter") {
+        handleCalClick()
+      } else {
+        setSum((prevSum) => prevSum + e.key);
+      }
+        
     
-    const handleKeyDown = (e)=> {
-        console.log(e)
-    }
+};
 
-    const handleRemoveClick = () => {
-        setResult('')
-        setSum('')
+  
+  const handleRemoveClick = () => {
+      setResult('');
+      setSum('');
+  };
 
-    }
-      
-    return (
-    <div className='Home-body'>
-        <div className='Home-Cal'>
-            <div className='Cal-Result'>{result}</div>
-            <div className='Cal-input'>{sum}</div>
-                <div>
-                    <div>
-                        <span value="=" onClick={handleCalClick}>=</span>
-                        <span value="/" onClick={handleNumClick}>/</span>
-                        <span value="*" onClick={handleNumClick}>*</span>
-                        <span value="A/C" onClick={handleRemoveClick}>C</span>
-                    </div>
-                    <div>
-                        <span onChange={handleKeyDown} value={1} onClick={handleNumClick} >1</span>
-                        <span value={2} onClick={handleNumClick}>2</span>
-                        <span value={3} onClick={handleNumClick}>3</span>
-                        <span value={0} onClick={handleNumClick}>0</span>
-                    </div>
-                    <div>    
-                        <span value={4} onClick={handleNumClick}>4</span>
-                        <span value={5} onClick={handleNumClick}>5</span>
-                        <span value={6} onClick={handleNumClick}>6</span>
-                        <span value="-" onClick={handleNumClick}>-</span>
-                    </div>
-                    <div>
-                        <span value={7} onClick={handleNumClick}>7</span>
-                        <span value={8} onClick={handleNumClick}>8</span>
-                        <span value={9} onClick={handleNumClick}>9</span>
-                        <span value="+" onClick={handleNumClick}>+</span>
-                    </div>
-                 </div>       
-        </div>
-    </div>
-  )
+
+    
+  return (
+      <div className='Home-body'>
+          <div className='Home-Cal'>
+              <div className='Cal-Result'>{result}</div>
+              <div className='Cal-input'>{sum}</div>
+              <div>
+                  <div>
+                      <span data-value="=" onClick={handleCalClick}>=</span>
+                      <span data-value="/" onClick={handleNumClick}>/</span>
+                      <span data-value="*" onClick={handleNumClick}>*</span>
+                      <span data-value="A/C" onClick={handleRemoveClick}>C</span>
+                  </div>
+                  <div>
+                      <span data-value={1} onClick={handleNumClick}>1</span>
+                      <span data-value={2} onClick={handleNumClick}>2</span>
+                      <span data-value={3} onClick={handleNumClick}>3</span>
+                      <span data-value={0} onClick={handleNumClick}>0</span>
+                  </div>
+                  <div>    
+                      <span data-value={4} onClick={handleNumClick}>4</span>
+                      <span data-value={5} onClick={handleNumClick}>5</span>
+                      <span data-value={6} onClick={handleNumClick}>6</span>
+                      <span data-value="-" onClick={handleNumClick}>-</span>
+                  </div>
+                  <div>
+                      <span data-value={7} onClick={handleNumClick}>7</span>
+                      <span data-value={8} onClick={handleNumClick}>8</span>
+                      <span data-value={9} onClick={handleNumClick}>9</span>
+                      <span data-value="+" onClick={handleNumClick}>+</span>
+                  </div>
+              </div>       
+          </div>
+      </div>
+  );
 }
-
-
-// 하나씩 지우기 + 리덕스 +  
